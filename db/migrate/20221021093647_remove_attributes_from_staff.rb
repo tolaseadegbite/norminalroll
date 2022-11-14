852a4f0fc9f5562b5e6c5bdc6d2d078e1d91f512
tolaseadegbite <tolasekelvin@gmail.com>
2022-11-14 23:42:33 +0100

    Modify migration folder

--- db/migrate/20221021093647_remove_attributes_from_staff.rb
+++ db/migrate/20221021093647_remove_attributes_from_staff.rb
@@ -2,7 +2,7 @@ class RemoveAttributesFromStaff < ActiveRecord::Migration[7.0]
   def change
     remove_column :staffs, :title, :string
     remove_column :staffs, :staff_status, :string
-    remove_column :staffs, :status_of_app, :string
+    # remove_column :staffs, :status_of_app, :string
     remove_column :staffs, :sex, :string
   end
 end
